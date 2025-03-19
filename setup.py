b6cc1d0bd51f531f932596a17a7dd0cde7b4db52
Jasmine_Waller <vwx5pn@virginia.edu>
2025-03-18 22:42:07 -0400

    Mod 09 Challenge

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='Demo',
+    version='0.1',
+    description='A simple demonstration package',
+    url='https://github.com/jwall-uva/repo-practice#',
+    author='Jasmine Waller',
+    author_email='vwx5pn@virginia.edu',
+    license='MIT',
+    packages=['demo']
+)
